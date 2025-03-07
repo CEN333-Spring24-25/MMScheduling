7855da1367fbe3d4fd08863c5ccd467266af4cf7
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-03-08 03:28:33 +0400

    Drag and Drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
 import { CENProgramService } from './services/CENProgramService'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,DragDropModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MMScheduling';
-  constructor(cen: CENProgramService){
-    console.log(cen.getProgram());
+  list1 = ['Item 1', 'Item 2', 'Item 3'];
+  list2 = ['Item 4', 'Item 5'];
 
-  } 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      // Move item within the same list
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      // Move item to the other list
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
