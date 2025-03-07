59ce57ad41ec33fd8a3feef43eae207e6555f51d
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-03-08 03:41:58 +0400

    Drag and drop of section like objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CENProgramService } from './services/CENProgramService';
+import { Course } from './data/course';
+import { Faculty } from './data/faculty';
+import { Section } from './data/section';
 
-import { CENProgramService } from './services/CENProgramService'
+export interface Item {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [CommonModule,DragDropModule],
@@ -11,10 +18,16 @@ import { CENProgramService } from './services/CENProgramService'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  list1 = ['Item 1', 'Item 2', 'Item 3'];
-  list2 = ['Item 4', 'Item 5'];
+  list1: Section[] = [
+    new Section('001', new Course('CEN305','Math 101',3), new Faculty('1', 'Dr. Smith', 12, 0, 'AD')),
+    new Section('002', new Course('CEN325','Math 102',3), new Faculty('2', 'Dr. Medo', 12, 0, 'AD')),
+    new Section('003', new Course('CEN405','Math 103',3), new Faculty('3', 'Dr. Ala', 12, 0, 'AD'))
+  ];
 
-  drop(event: CdkDragDrop<string[]>) {
+  list2: Section[] = [
+  ];
+
+  drop(event: CdkDragDrop<Section[]>) {
     if (event.previousContainer === event.container) {
       // Move item within the same list
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
